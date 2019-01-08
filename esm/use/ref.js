febedda57d5932af5cf2041aaeab374e514322db
Andrea Giammarchi <a.giammarchi@eyeo.com>
2019-01-08 10:47:26 +0100

    dropped unnecessary parens

--- esm/use/ref.js
+++ esm/use/ref.js
@@ -4,7 +4,7 @@ const id = uid();
 
 setup.push(stacked(id));
 
-export default (value) => {
+export default value => {
   const {[id]: state} = current();
   const {i, stack} = state;
   state.i = i < stack.length ? (i + 1) : stack.push({current: $(value)});
